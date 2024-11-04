02a989c032c060920162d302214213e9a9d8a364
moumou <1482283801@qq.com>
2024-11-04 20:00:23 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,53 @@
+<template>
+   <div id="app" style="width: 500px; height: 500px;">
+    <link rel="stylesheet" href="../stores/index.css" />
+        <div style="margin-bottom:20px; width: max-content; height: 50px; margin-top: 120px;">
+            <SButton color="blue">主要按钮</SButton>
+            <SButton color="green">绿色按钮</SButton>
+            <SButton color="gray">灰色按钮</SButton>
+            <SButton color="yellow">黄色按钮</SButton>
+            <SButton color="red">红色按钮</SButton>
+        </div>
+      <br>
+        <div style="margin-bottom:20px;  width: max-content; height: 50px;  margin-top: -80px;">
+            <SButton color="blue" plain>朴素按钮</SButton>
+            <SButton color="green" plain>绿色按钮</SButton>
+            <SButton color="gray" plain>灰色按钮</SButton>
+            <SButton color="yellow" plain>黄色按钮</SButton>
+            <SButton color="red" plain>红色按钮</SButton>
+        </div>
+        <br>
+        <div style="margin-bottom:20px;  width: max-content; height: 50px; margin-top: -90px;">
+            <SButton size="small" plain>小按钮</SButton>
+            <SButton size="medium" plain>中按钮</SButton>
+            <SButton size="large" plain>大按钮</SButton>
+        </div>
+        <br>
+        <div style="margin-bottom:20px;  width: max-content; height: 50px; margin-top: -80px;">
+            <SButton color="blue" round plain icon="search">搜索按钮</SButton>
+            <SButton color="green" round plain icon="edit">编辑按钮</SButton>
+            <SButton color="gray" round plain icon="check">成功按钮</SButton>
+            <SButton color="yellow" round plain icon="message">提示按钮</SButton>
+            <SButton color="red" round plain icon="delete">删除按钮</SButton>
+        </div>
+        <br>
+        <div style="margin-bottom:20px;  width: max-content; height: 50px; margin-top: -90px;">
+            <SButton color="blue" round plain icon="search"></SButton>
+            <SButton color="green" round plain icon="edit"></SButton>
+            <SButton color="gray" round plain icon="check"></SButton>
+            <SButton color="yellow" round plain icon="message"></SButton>
+            <s-button color="red" round plain icon="delete"></s-button>
+        </div>
+      </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+</style>
